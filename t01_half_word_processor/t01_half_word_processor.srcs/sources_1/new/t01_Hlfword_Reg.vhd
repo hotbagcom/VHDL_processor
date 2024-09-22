30c24ba367524eea4dd6f52fa9de6d3a15100fc8
Arif Yardım <96440900+hotbagcom@users.noreply.github.com>
2024-09-22 21:07:00 +0300

    ram array and architecture wroted

--- t01_half_word_processor/t01_half_word_processor.srcs/sources_1/new/t01_Hlfword_Reg.vhd
+++ t01_half_word_processor/t01_half_word_processor.srcs/sources_1/new/t01_Hlfword_Reg.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,6 +32,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity t01_Hlfword_Reg is
+    Generic(
+           Ram_width : integer := 4;
+           Ram_depth :integer := 16
+    );
     Port ( clk : in STD_LOGIC := '0';
            rst_ah : in  STD_LOGIC := '0';
            Enable_Writedata_reg_in0 : in STD_LOGIC := '0';
@@ -45,8 +49,21 @@ entity t01_Hlfword_Reg is
 end t01_Hlfword_Reg;
 
 architecture bhvrl_Reg of t01_Hlfword_Reg is
+--TO DO  diferrence btw stdlogicvector and bitvector
+type ram_type is array(0 to Ram_depth-1) of std_logic_vector(Ram_width-1 downto 0);
+signal RAM_Reg : ram_type := (others=>(others=>'0'));
+
 
 begin
-
-
+process (clk) begin 
+    if(rst_ah ='1') then
+    RAM_Reg  <= (others=>(others=>'0'));
+    elsif rising_edge(clk )then 
+    Reg_out0 <=  RAM_Reg( to_integer(unsigned(Source_in0)) );
+    Reg_out1 <=  RAM_Reg( to_integer(unsigned(Source_in1)) );
+        if ( Enable_Writedata_reg_in0 = '1') then
+        RAM_Reg(to_integer(unsigned(Destination_in0))) <= Writedata_in0 ;
+        end if;
+    end if;
+end process ;
 end bhvrl_Reg;
